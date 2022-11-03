f9788b4229e8ee4477534d28120cfbbaff4d6e2f
Tim Chen <29265905+timyc@users.noreply.github.com>
2022-11-03 15:46:57 -0700

    typo fix

--- src/helpers/Struct.ts
+++ src/helpers/Struct.ts
@@ -1,6 +1,6 @@
-export function DegreeData(highest_degree:number = 0, major_name:string = 'Unknown', gpa:number = 0.0, begin_date:string = '0000-00-00', end_date:string = '0000-00-00') {
+export function DegreeData(highest_degree:number = 0, degree_name:string = 'Unknown', gpa:number = 0.0, begin_date:string = '0000-00-00', end_date:string = '0000-00-00') {
     this.highest_degree = highest_degree;
-    this.major_name = major_name;
+    this.degree_name = degree_name;
     this.gpa = gpa;
     this.begin_date = begin_date;
     this.end_date = end_date;
